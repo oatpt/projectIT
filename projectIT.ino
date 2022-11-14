046b60e3c5a33c8835d2013253e507af8ad8eddc
oatpt <75560254+oatpt@users.noreply.github.com>
2022-11-14 14:51:40 +0700

    v1

--- projectIT.ino
+++ projectIT.ino
@@ -18,85 +18,63 @@ void callback(char *topic, byte *payload, unsigned int length) {
   text="";
   for (int i = 0; i < length; i++) 
      text+=(char) payload[i];
-  if(strcmp(topic,"projectit/F2/202")==0)
-  {
-
+  if(strcmp(topic,"projectit/F5/501/door")==0){
+    Serial.printf("hi");
     if(text.compareTo("true")==0)
-      
       digitalWrite(4,HIGH);
     else
       digitalWrite(4,LOW);
-      
   }
-  
-
+  if(strcmp(topic,"projectit/F5/501/reboot")==0){
+    Serial.printf("hello");
+    if(text.compareTo("true")==0)
+      ESP.restart();   
+  }
 }
 
 void reconnect() { 
-
       client.setServer(mqtt_broker, mqtt_port);            //set mqtt server ตามIPและportที่ตั้งไว้
-
       client.setCallback(callback);                              //เลือกฟังก์ชั่นsubscribeจากหน้า12
-
-      while (!client.connected())                                //รอจนกว่าจะเชื่อมต่อmqttสำเร็จ
-
-      {
-
+      while (!client.connected()){                                //รอจนกว่าจะเชื่อมต่อmqttสำเร็จ
             String client_id = "esp32-nut-";
-
             client_id += String(WiFi.macAddress());
-
             Serial.printf("The client %s connects to the public mqtt broker\n", client_id.c_str());
-
-            
-
             if (client.connect(client_id.c_str(), mqtt_username, mqtt_password))
-
                   Serial.println("Public emqx mqtt broker connected");
-
             else {
-
                   Serial.print("failed with state ");
-
                   Serial.print(client.state());
-
                   delay(2000);
-
-}     }     }
-
-
-void setup_wifi() { 
-
-      WiFi.mode(WIFI_STA); 
-
-      WiFi.begin(ssid, password); 
-
-      while (WiFi.status() != WL_CONNECTED) {
-
-            delay(500);
-
-            Serial.println("Connecting to WiFi...");
-
-      }
-
-      Serial.println("WiFi connected");
-
+            }     
+      }     
 }
-
+void setup_wifi() { 
+      WiFi.mode(WIFI_STA); 
+      WiFi.begin(ssid, password); 
+      while (WiFi.status() != WL_CONNECTED) {
+            delay(500);
+            Serial.println("Connecting to WiFi...");
+      }
+      Serial.println("WiFi connected");
+}
 void setup() {
   // put your setup code here, to run once:
   pinMode(4,OUTPUT);
   Serial.begin(115200);
   Serial.print("nut");
   setup_wifi();
-
-reconnect();                               //เชื่อมต่อmqtt
-
-client.subscribe("projectit/F2/202");       //กำหนด topic ที่จะ subscribe
-
+  reconnect();                               //เชื่อมต่อmqtt
+  client.subscribe("projectit/F5/501/door");       //กำหนด topic ที่จะ subscribe
+  client.subscribe("projectit/F5/501/reboot");
+  client.publish("projectit/F5/501/init","true");
 }
-
+long timedelay;
 void loop() {
   // put your main code here, to run repeatedly:
   client.loop();
+  if(millis()-timedelay>=5000)
+  {
+    timedelay= millis();
+    client.publish("projectit/F5/501/timeupdate","true");
+  }
 }
